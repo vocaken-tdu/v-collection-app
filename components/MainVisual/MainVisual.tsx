111a6c11692c0f3161a188ed1204f8879a14a0cd
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-06 04:10:59 +0900

    GSAPの導入

--- components/MainVisual/MainVisual.tsx
+++ components/MainVisual/MainVisual.tsx
@@ -1,11 +1,26 @@
 'use client';
 
+import { useLayoutEffect, useRef } from 'react';
 import { Image, Container, Group } from '@mantine/core';
+import { gsap } from 'gsap';
 import image from '../../public/illustrations/202311221521.png';
 import logo from '../../public/Logo.png';
 import classes from './MainVisual.module.css';
 
 export function MainVisual() {
+  const boxRef = useRef();
+
+  // 上下に動くアニメーション
+  useLayoutEffect(() => {
+    gsap.to(boxRef.current, {
+      y: '+=12',
+      duration: 1.8,
+      ease: 'sine.inOut',
+      repeat: -1,
+      yoyo: true,
+    });
+  });
+
   return (
     <Container size="xl" className={`${classes.main} h-screen overflow-hidden`}>
       <div className={classes.inner}>
@@ -24,7 +39,7 @@ export function MainVisual() {
             />
           </Group>
         </div>
-        <div className={`${classes.wrap} rotate-3`}>
+        <div className={`${classes.wrap} rotate-3`} ref={boxRef}>
           <Image
             className={classes.image}
             src={image.src}
