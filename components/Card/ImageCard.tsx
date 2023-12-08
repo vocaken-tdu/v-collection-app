81577c0f996dd0a9c23a191ddc41341a0f4268cb
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-08 17:34:22 +0900

    APIから取得するように変更(仮)

--- components/Card/ImageCard.tsx
+++ components/Card/ImageCard.tsx
@@ -1,83 +1,88 @@
 'use client';
 
+import React, { useState, useEffect } from 'react';
 import { IconMessageCircle } from '@tabler/icons-react';
 import { Card, Text, Group, Center, rem, useMantineTheme } from '@mantine/core';
+import axios, { AxiosResponse } from 'axios';
 import classes from './ImageCard.module.css';
 
-// 仮置き
-const illustInfo = [
-  {
-    id: 1,
-    artist: 'Nyanya',
-    src: '../../illustrations/202311221521.png',
-    comment: 12,
-  },
-  {
-    id: 2,
-    artist: 'sigre-33',
-    src: '../../illustrations/202311221724.png',
-    comment: 3,
-  },
-  {
-    id: 3,
-    artist: 'spring-s',
-    src: '../../illustrations/202311221725.png',
-    comment: 122,
-  },
-  {
-    id: 4,
-    artist: 'Rinren',
-    src: '../../illustrations/202311221730.png',
-    comment: 1236,
-  },
-];
+const apiUrl = 'https://vcollection-background.onrender.com/illustrations';
+
+type IllustInfo = {
+  title: string;
+  description: string;
+  url: string;
+  id: 0;
+  created_at: string;
+  comments: [
+    {
+      text: string;
+      illustration_id: number;
+      user_id: number;
+      id: number;
+    },
+  ];
+  likes: number;
+};
 
 export function ImageCard() {
   const theme = useMantineTheme();
 
+  const [illusts, setIllusts] = useState<IllustInfo[]>([]);
+
+  useEffect(() => {
+    const fetchIllusts = async () => {
+      const response: AxiosResponse<IllustInfo[]> = await axios.get(apiUrl);
+      setIllusts(response.data);
+      console.log(response.data);
+    };
+    fetchIllusts();
+  }, []);
+
   return (
-    <div className={classes.cards}>
-      {illustInfo.map((illust) => (
-        <div>
-          <Card
-            p="lg"
-            shadow="lg"
-            className={classes.card}
-            radius="md"
-            component="a"
-            href="https://mantine.dev/"
-            target="_blank"
-          >
-            <div
-              className={classes.image}
-              style={{
-                backgroundImage:
-                  `url(${illust.src})`,
-              }}
-            />
-            <div className={classes.overlay} />
-          </Card>
+    <>
+      <div className={classes.cards}>
+        {illusts.map((illust, i) => (
+          <div key={i}>
+            <Card
+              p="lg"
+              shadow="lg"
+              className={classes.card}
+              radius="md"
+              component="a"
+              href={`/illustrations/${illust.id}`}
+              target="_blank"
+            >
+              <div
+                className={classes.image}
+                style={{
+                  backgroundImage: `url(https://via.placeholder.com/600x800/?text=id%E2%86%92${illust.id}%0Adesc%E2%86%92${illust.description})`,
+                }}
+              />
+              <div className={classes.overlay} />
+            </Card>
 
-          <div className={`${classes.content} mt-2`}>
-            <Group justify="space-between" gap="xs">
-              <Text size="sm" className={classes.artist}>
-                {illust.artist}
-              </Text>
-
-              <Center>
-                <IconMessageCircle
-                  style={{ width: rem(16), height: rem(16) }}
-                  stroke={1.5}
-                  color={theme.colors.dark[2]}
-                />
-                <Text size="sm" className={classes.bodyText}>
-                  {illust.comment}
+            <div className={`${classes.content} mt-2`}>
+              <Group justify="space-between" gap="xs">
+                <Text size="sm" className={classes.artist}>
+                  {illust.title}
                 </Text>
-              </Center>
-            </Group>
+
+                <Center>
+                  <IconMessageCircle
+                    style={{ width: rem(16), height: rem(16) }}
+                    stroke={1.5}
+                    color={theme.colors.dark[2]}
+                  />
+                  <Text size="sm" className={classes.bodyText}>
+                    {illust.description}
+                  </Text>
+                </Center>
+              </Group>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
