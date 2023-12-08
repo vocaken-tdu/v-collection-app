104a349626a560f5c4647e45d2d7b48c323f9158
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-09 01:37:31 +0900

    [bug fix] works/idページの仮実装

--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Container } from '@mantine/core';
 import { BigImageCard } from '../../../components/Card/BigImageCard';
 
-export default function Works({ params }: { params: { id: string } }) {
+export default function Works({ params }: { params: { id: number } }) {
   return (
     <>
       <Container size="xl">
