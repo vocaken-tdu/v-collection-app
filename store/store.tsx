7e3c23b27b2d978bbbb6c65daabd66af29fbfb8f
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-10 13:36:04 +0900

    storeを取得ごと/ロジックごとにわける

--- store/store.tsx
+++ store/store.tsx
@@ -3,7 +3,9 @@ import axios, { AxiosResponse } from 'axios';
 
 const apiUrl = 'https://vcollection-background.onrender.com/illustrations';
 
-type Store = {
+// -------- イラスト一覧を取得する
+
+type illustListStore = {
   illustList: [
     {
       title: string;
@@ -22,25 +24,9 @@ type Store = {
       likes: number;
     },
   ];
-  illust: {
-    title: string;
-    description: string;
-    url: string;
-    id: 0;
-    created_at: string;
-    comments: [
-      {
-        text: string;
-        illustration_id: number;
-        user_id: number;
-        id: number;
-      },
-    ];
-    likes: number;
-  };
 };
 
-export const useBearStore = create<Store>()(() => ({
+export const useIllustList = create<illustListStore>()(() => ({
   illustList: [
     {
       title: 'title',
@@ -59,6 +45,40 @@ export const useBearStore = create<Store>()(() => ({
       likes: 0,
     },
   ],
+}));
+
+export const setIllustList = async () => {
+  const fetch = async () => {
+    const response: AxiosResponse = await axios.get(apiUrl);
+    useIllustList.setState({ illustList: response.data });
+    console.log('Data is fetched!');
+  };
+  const fetchState = useIllustList.getState().illustList;
+  fetchState.length < 2 ? fetch() : console.log('Data is already fetched!');
+};
+
+// -------- イラストを取得する
+
+type illustStore = {
+  illust: {
+    title: string;
+    description: string;
+    url: string;
+    id: 0;
+    created_at: string;
+    comments: [
+      {
+        text: string;
+        illustration_id: number;
+        user_id: number;
+        id: number;
+      },
+    ];
+    likes: number;
+  };
+};
+
+export const useIllust = create<illustStore>()(() => ({
   illust: {
     title: 'title',
     description: 'description',
@@ -77,22 +97,44 @@ export const useBearStore = create<Store>()(() => ({
   },
 }));
 
-export const setIllustList = async () => {
-  const fetch = async () => {
-    const response: AxiosResponse = await axios.get(apiUrl);
-    useBearStore.setState({ illustList: response.data });
-    console.log('Data is fetched!');
-  };
-  const fetchState = useBearStore.getState().illustList;
-  fetchState.length < 2 ? fetch() : console.log('Data is already fetched!');
-};
-
 export const setIllust = async (id: number) => {
   const fetch = async () => {
     const response: AxiosResponse = await axios.get(`${apiUrl}/%7Billustration_id%7D?id=${id}`);
-    useBearStore.setState({ illust: response.data });
+    useIllust.setState({ illust: response.data });
     console.log('illustData is fetched!');
   };
-  const fetchState = useBearStore.getState().illust;
-  fetchState ? fetch() : console.log('Data is already fetched!');
+  const fetchState = useIllust.getState().illust;
+  fetchState ? fetch() : console.log('illustData is already fetched!');
+};
+
+// -------- コメントを取得する
+
+type commentStore = {
+  comment: {
+    text: 'string';
+    illustration_id: number;
+    user_id: number;
+    id: number;
+    likes: number;
+  };
+};
+
+export const useComment = create<commentStore>()(() => ({
+  comment: {
+    text: 'string',
+    illustration_id: 0,
+    user_id: 0,
+    id: 0,
+    likes: 0,
+  },
+}));
+
+export const setComment = async (id: number) => {
+  const fetch = async () => {
+    const response: AxiosResponse = await axios.get(`${apiUrl}/comments/${id}`);
+    useComment.setState({ comment: response.data });
+    console.log('commentData is fetched!');
+  };
+  const fetchState = useComment.getState().comment;
+  fetchState ? fetch() : console.log('commentData is already fetched!');
 };
