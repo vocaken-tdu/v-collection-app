b75abcf0e2baffb62ce736ae2cfa51db7b56b4bc
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-12 02:30:29 +0900

    init

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Vコレってなんだ？</h1>
-      <p>ああああ</p>
-    </>
-  );
-}
--- app/fonts.tsx
+++
@@ -1,7 +0,0 @@
-import { Noto_Sans_JP } from 'next/font/google';
-
-export const notojp = Noto_Sans_JP({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-notojp',
-});
