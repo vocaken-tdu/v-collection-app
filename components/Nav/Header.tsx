ba677a61ecd394bb9ad9ae0fb3f58bd7d76bb5b6
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-15 11:53:41 +0900

    lint / 使用してない関数を削除

--- components/Nav/Header.tsx
+++ components/Nav/Header.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Container, Group, Burger, Drawer, Text} from '@mantine/core';
+import React from 'react';
+import { Container, Group, Burger, Drawer, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Abel } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 import classes from './Header.module.css';
-import { usePathname } from "next/navigation";
-import path from 'path';
 
 export const abel = Abel({
   weight: ['400'],
@@ -21,30 +20,22 @@ const links = [
   { link: 'https://www.vocakentdu.com/', label: 'VOCALOID同好会' },
 ];
 
-
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
-  const [act_ive, setActive] = useState<string | null>(null);
   const pathname = usePathname();
-  const active = pathname
-  
-  console.log('%s', pathname);
+  const active = pathname;
 
   const items = links.map((link) => (
     <a
-    key={link.label}
-    href={link.link}
-    className={classes.link}
-    data-active={active === link.link || undefined}
-    onClick={(event) => {
-      setActive(link.link);
-      //event.preventDefault();
-    }}
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
     >
       {link.label}
     </a>
   ));
-  
+
   return (
     <header className={classes.header}>
       <Container size="xl" className={classes.inner}>
@@ -57,9 +48,9 @@ export function Header() {
         </Group>
 
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        <Drawer opened={opened} onClose={toggle} position='right' className={classes.Burger}>
-            <Text style={{ fontSize: 23, fontWeight: '500', height:50 }}>Menu</Text>
-            {items}
+        <Drawer opened={opened} onClose={toggle} position="right" className={classes.burger}>
+          <Text style={{ fontSize: 23, fontWeight: '500', height: 50 }}>Menu</Text>
+          {items}
         </Drawer>
       </Container>
     </header>
