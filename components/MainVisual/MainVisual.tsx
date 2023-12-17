d4c49e5a091a85e8554b0c4497fabe193fd8901c
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-17 19:27:07 +0900

    MainVisualがランダムに画像を表示するように(仮)

--- components/MainVisual/MainVisual.tsx
+++ components/MainVisual/MainVisual.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useLayoutEffect, useRef } from 'react';
-import { Image, Container, Group, Text } from '@mantine/core';
+import { useEffect, useLayoutEffect, useRef } from 'react';
+import { Image, Container, Group, Skeleton } from '@mantine/core';
 import { gsap } from 'gsap';
-import image from '@/public/illustrations/202311221521.png';
 import logo from '@/public/Logo.png';
 import classes from './MainVisual.module.css';
+import { setIllustList, useIllustList } from '@/store/illustListStore';
 
 export function MainVisual() {
   const boxRef = useRef(null);
@@ -19,15 +19,32 @@ export function MainVisual() {
       repeat: -1,
       yoyo: true,
     });
+    gsap.to('#randomImage', {
+      opacity: 1,
+      duration: 1,
+      ease: 'sine.inOut',
+    });
   });
-  
+
+  // イラスト(リスト)を取得
+  useEffect(() => {
+    setIllustList();
+  }, []);
+
+  // イラスト(リスト)の状態を取得
+  const illusts = useIllustList((state) => state.illustList);
+
+  // 取得済みかどうかを判定
+  const isFetched = useIllustList((state) => state.isFetched);
+
   return (
     <Container size="xl" className={`${classes.main} h-screen overflow-hidden`}>
       <div className={classes.inner}>
         <div className={`${classes.content} grid place-content-center`}>
           <div className={classes.title}>
             <div className={`${classes.highlight} ${classes.l}`}>あのキャラはこの冬</div>
-            <div className={`${classes.highlight} ${classes.r}`}>どんな服
+            <div className={`${classes.highlight} ${classes.r}`}>
+              どんな服
               <div className={classes.pcText}>で過ごしているだろう</div>
               <div className={classes.spText}>なんだろう</div>
             </div>
@@ -38,7 +55,9 @@ export function MainVisual() {
         </div>
         {/*メインイラスト*/}
         <div className="rotate-3" ref={boxRef}>
-          <Image className={classes.image} src={image.src} />
+          <Skeleton visible={!isFetched}>
+            <Image id="randomImage" className={classes.image} src={`${isFetched ? illusts[Math.floor(Math.random() * illusts.length)].illust : undefined} `} />
+          </Skeleton>
         </div>
 
         {/*ロゴ2*/}
