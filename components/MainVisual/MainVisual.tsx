56a45de128b7095d79a380df63cd08d84089e601
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-17 18:41:26 +0900

    MainVisualのブレークポイントを微調整 / タブレット他機種のレスポンシブ対応

--- components/MainVisual/MainVisual.tsx
+++ components/MainVisual/MainVisual.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useLayoutEffect, useRef } from 'react';
-import { Image, Container, Group, Text} from '@mantine/core';
+import { Image, Container, Group, Text } from '@mantine/core';
 import { gsap } from 'gsap';
 import image from '@/public/illustrations/202311221521.png';
 import logo from '@/public/Logo.png';
@@ -20,43 +20,31 @@ export function MainVisual() {
       yoyo: true,
     });
   });
-
+  
   return (
     <Container size="xl" className={`${classes.main} h-screen overflow-hidden`}>
       <div className={classes.inner}>
         <div className={`${classes.content} grid place-content-center`}>
           <div className={classes.title}>
-            <div className={`${classes.highlight} ${classes.l}`}>
-              あのキャラはこの冬
-            </div>
-            <div className={`${classes.highlight} ${classes.r}`}>
-              どんな服で過ごしているだろう
+            <div className={`${classes.highlight} ${classes.l}`}>あのキャラはこの冬</div>
+            <div className={`${classes.highlight} ${classes.r}`}>どんな服
+              <div className={classes.pcText}>で過ごしているだろう</div>
+              <div className={classes.spText}>なんだろう</div>
             </div>
           </div>
-          <Group mt={60} visibleFrom="sm">
-            <Image
-              className={classes.logo}
-              src={logo.src}
-              />
+          <Group mt={64} visibleFrom="md">
+            <Image className={classes.logo} src={logo.src} />
           </Group>
         </div>
         {/*メインイラスト*/}
-        <div className={`${classes.wrap} rotate-3`} ref={boxRef}>
-          <Image
-            className={classes.image}
-            src={image.src}
-            />
+        <div className="rotate-3" ref={boxRef}>
+          <Image className={classes.image} src={image.src} />
         </div>
-        
+
         {/*ロゴ2*/}
-        <div>
-          <Group mt={60} hiddenFrom="sm">
-            <Image
-              className={classes.logo}
-              src={logo.src}
-            />
-          </Group>
-        </div>
+        <Group mt={64} hiddenFrom="md" justify="center">
+          <Image className={classes.logo} src={logo.src} />
+        </Group>
       </div>
     </Container>
   );
