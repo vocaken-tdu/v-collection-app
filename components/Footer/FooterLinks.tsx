e2d0bbbc5f1abdb0cc87a938ba15c1148b870325
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-18 12:13:01 +0900

    Footerの他機種レスポンシブ対応

--- components/Footer/FooterLinks.tsx
+++ components/Footer/FooterLinks.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Text, Container, ActionIcon, Group, Button, rem } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconArrowBack } from '@tabler/icons-react';
+import { IconBrandTwitterFilled, IconBrandYoutube, IconArrowBack } from '@tabler/icons-react';
 import classes from './FooterLinks.module.css';
 
 //呼び出されたら出力する場所
@@ -9,17 +9,16 @@ export function FooterLinks() {
   return (
     <div className={classes.footerWrap}>
       <footer className={classes.footer}>
-        <Container className={classes.inner} size="lx">
+        <Container size="xl">
           <Button
             component="a"
-            href="/"
+            href="https://akatukime.wixsite.com/collection/"
             variant="filled"
             rightSection={
               <IconArrowBack style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
             }
             radius="xl"
-            size="xl"
-            className={classes.logo}
+            size="lg"
             styles={{
               root: { paddingRight: rem(14), height: rem(48) },
               section: { marginLeft: rem(22) },
@@ -29,60 +28,56 @@ export function FooterLinks() {
           </Button>
         </Container>
 
-        <Container className={classes.afterFooter} size="lx">
-          <div className={classes.test1}>
-            <Text c="dark" style={{ fontSize: 23, fontWeight: '500' }}>
-              東京電機大学
-            </Text>
-            <Text c="dark" style={{ fontSize: 30, fontWeight: '500' }}>
-              VOCALOID同好会
-            </Text>
-          </div>
-
-          {/*アイコン関係の場所*/}
-          <section>
-            <div className={classes.test2}>
-              <Text style={{ fontSize: 23 }}>Share</Text>
-
-              <Group gap={0} className={classes.social} wrap="nowrap" justify="space-between">
-                <ActionIcon size="lg" color="dark" variant="subtle">
-                  <a href="" className={classes.icon}>
-                    <IconBrandTwitter style={{ width: 20, height: 20 }} stroke={1.5} />
-                  </a>
-                </ActionIcon>
-
-                <ActionIcon size="lg" color="dark" variant="subtle">
-                  <a href="https://www.nicovideo.jp/watch/sm39776265" className={classes.icon}>
-                    <IconBrandYoutube style={{ width: 20, height: 20 }} stroke={1.5} />
-                  </a>
-                </ActionIcon>
-              </Group>
+        <Container className={classes.afterFooter} size="xl">
+          <Group gap={32} justify="flex-start">
+            <div className={classes.tdu}>
+              <Text c="dark" style={{ fontSize: 23, fontWeight: '500' }}>
+                東京電機大学
+              </Text>
+              <Text c="dark" style={{ fontSize: 30, fontWeight: '500' }}>
+                VOCALOID同好会
+              </Text>
             </div>
 
-            <div className={classes.test2}>
-              <Text style={{ fontSize: 23 }}>Follow us</Text>
-              <Group gap={0} className={classes.social} justify="left" wrap="nowrap">
-                <ActionIcon size="lg" color="brack" variant="subtle">
-                  <a
-                    href="https://pbs.twimg.com/profile_images/1509818267812364288/Y1EMCVdD_400x400.jpg"
-                    className={classes.icon}
-                  >
-                    <IconBrandTwitter style={{ width: 20, height: 20 }} stroke={1.5} />
-                  </a>
-                </ActionIcon>
-              </Group>
-            </div>
-          </section>
+            {/*アイコン関係の場所*/}
+
+            <Group gap={28} wrap="nowrap" className={classes.icons}>
+              <div>
+                <Text style={{ fontSize: 23 }}>Share</Text>
+                <Group gap={8} className={classes.social} wrap="nowrap" justify="flex-start">
+                  <ActionIcon size="lg" color="dark" variant="subtle">
+                    <a
+                      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+                      className={classes.icon}
+                    >
+                      <IconBrandTwitterFilled style={{ width: 20, height: 20 }} stroke={1.5} />
+                    </a>
+                  </ActionIcon>
+                </Group>
+              </div>
+
+              <div>
+                <Text style={{ fontSize: 23 }}>Follow us</Text>
+                <Group gap={8} className={classes.social} wrap="nowrap" justify="flex-start">
+                  <ActionIcon size="lg" color="brack" variant="subtle">
+                    <a href="https://twitter.com/vocaken_tdu" className={classes.icon}>
+                      <IconBrandTwitterFilled style={{ width: 20, height: 20 }} stroke={1.5} />
+                    </a>
+                  </ActionIcon>
+                </Group>
+              </div>
+            </Group>
+          </Group>
 
           <div className={classes.right}>
-            <Text c="dark" style={{ fontSize: 14 }}>
+            <Text c="dark" style={{ fontSize: 14 }} className={classes.copyright}>
               「VOCALOID（ボーカロイド）」ならびに
-              <br />
+              <br className={classes.tbBr} />
               「ボカロ」はヤマハ株式会社の登録商標です。
-              <br />
+              <br className={classes.tbBr} />
               <br />
               © 2023 VOCALOID CLUB COLLECTION.
-              <br />
+              <br className={classes.spBr} />
               複製・転載等を固く禁じます
             </Text>
           </div>
