30fe350066ebc9ac029b4075b75fbc1840b4af12
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-18 12:27:35 +0900

    Aboutページの細かな調整

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,18 +5,25 @@ export default function Home() {
     <>
       <Container size="xl" className="text-center">
         <h2 className="text-4xl my-40">Vコレってなんだ？</h2>
-        <p className="text-xl my-20">
+        <p className="text-xl my-20 leading-loose">
           「ボカロキャラ(広義)がリアルにいたらどんな風に過ごしているのだろう？」
         </p>
-        <p className="text-xl my-20">
+        <p className="text-xl my-20 leading-loose">
           「ボカロモチーフのおしゃれ全般のグッズがあったらどんなものだろう？」
         </p>
-        <p className="text-xl my-40">
-          そんな想像を膨らませながら、<br />VOCALOID同好会の有志のメンバーが集まり
-          季節ごとにテーマを決めて絵を描いています。
+        <p className="text-xl my-40 leading-loose">
+          そんな想像を膨らませながら
+          <br />
+          VOCALOID同好会の有志のメンバーが集まり
+          <br />
+          季節ごとにテーマを決めて絵を描いています
         </p>
-        <p className="text-2xl mt-96 mb-20">
-          さて、今シーズンは、どんな服を着ているでしょうか？
+        <p className="text-2xl mt-96 mb-20 leading-relaxed">
+          さて……
+          <br /><br />
+          今シーズンは
+          <br />
+          どんな服を着ているでしょうか？
         </p>
         {/* 見てみる！ ボタンを表示 */}
         <Button
@@ -30,7 +37,6 @@ export default function Home() {
         >
           見てみる！
         </Button>
-
       </Container>
     </>
   );
