d6a3e7b86925c561b4305d07997cd2a68522cf84
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-22 02:09:26 +0900

    使い方ページ下に注意事項の追加

--- app/howto/page.tsx
+++ app/howto/page.tsx
@@ -7,12 +7,14 @@ export default function Home() {
       <Shape20 size="half" />
       <Container size="xl" className="text-center">
         <h2 className="text-4xl my-40">使い方</h2>
-        <h3 className="text-3xl mt-20">1. イラストを選ぼう</h3>
+
+        <h3 className="text-3xl mt-20">1. まずはイラストを見に行こう</h3>
         <p className="text-xl leading-relaxed">ピンときたイラストを見てみましょう。</p>
         <Button component="a" href="/" variant="outline" color="blue" radius="xl" size="lg">
           一覧を見てみる！
         </Button>
-        <h3 className="text-3xl mt-36">2. コメントしよう！</h3>
+
+        <h3 className="text-3xl mt-40">2. コメントしよう！</h3>
         <p className="text-xl leading-relaxed">思ったことを投稿してみましょう！ （部員限定）</p>
         <p className="text-xl leading-relaxed mt-16">例えば……</p>
         <Group justify="center" gap="md">
@@ -38,7 +40,8 @@ export default function Home() {
             冬っぽさ感じて好きです！」
           </Paper>
         </Group>
-        <h3 className="text-3xl mt-36">3. 他の人のコメントを見よう！</h3>
+
+        <h3 className="text-3xl mt-40">3. 他の人のコメントを見よう！</h3>
         <p className="text-xl leading-relaxed">
           Vコレでは、絵自体ではなく
           <br />
@@ -53,6 +56,23 @@ export default function Home() {
           <br />
           いいねを押してみましょう！ （一人1回まで！）
         </p>
+
+        <h2 className="text-4xl mt-80">注意事項</h2>
+
+        <h3 className="text-3xl mt-40">コメントは削除できないよ！</h3>
+        <p className="text-xl leading-relaxed">
+          原則コメントの編集や削除はできません。
+          <br />
+          コメントを投稿する前に、誤字をチェックしましょう！
+        </p>
+
+        <h3 className="text-3xl mt-40">ボタンは連打しちゃだめ！</h3>
+        <p className="text-xl leading-relaxed">
+          反映されていないように見えたら
+          <br />
+          一度ページを更新してみましょう。
+        </p>
+
       </Container>
     </>
   );
