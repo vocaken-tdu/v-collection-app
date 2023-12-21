1936d55d07d6e684709611aee19f2df576c7d7d8
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-21 20:54:09 +0900

    コメントの日時を何分前で表示できるように

--- components/Tools/GetRelativeTime.tsx
+++ components/Tools/GetRelativeTime.tsx
@@ -15,6 +15,9 @@ export function GetRelativeTime({ RawTime }: { RawTime: string }) {
     if (dayDiff < 0) {
       return '未来';
     }
+    if (diff < 60 * 60 * 1000) {
+      return `${Math.floor(diff / (60 * 1000))}分前`;
+    }
     if (dayDiff < 1) {
       return `${Math.floor(diff / (60 * 60 * 1000))}時間前`;
     }
