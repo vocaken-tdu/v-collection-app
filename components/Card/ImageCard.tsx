f8eb35563680f2641212fa72a43c58ed53c2ce75
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-23 16:50:30 +0900

    近日公開に背景とアニメーションを追加

--- components/Card/ImageCard.tsx
+++ components/Card/ImageCard.tsx
@@ -8,6 +8,7 @@ import { useIllustList, setIllustList } from '@/store/illustListStore';
 import { useTags, setTags } from '@/store/tagsStore';
 import { GetUserName } from '../Tools/GetUserName';
 import classes from './ImageCard.module.css';
+import flowShape from '@/public/flowShape.svg';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,6 +34,18 @@ export function ImageCard() {
           trigger: '#cards',
           start: 'top 80%',
           toggleActions: 'play none none reverse',
+        },
+      }
+    );
+    gsap.to(
+      '#commingSoon',
+      {
+        y: -100,
+        scrollTrigger: {
+          trigger: '#commingSoon',
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: 0.5,
         },
       }
     );
@@ -85,8 +98,24 @@ export function ImageCard() {
           </div>
           {/* イラストがない場合は公開予定であることを表示 */}
           {illusts.filter((illust) => illust.tags.includes(tag.id)).length === 0 && (
-            <Paper shadow="lg" radius="md" p={64} variant="light" className="text-center">
-              <Text size="xl">近日公開</Text>
+            // 背景にflowShapeを使用
+            <Paper
+              shadow="lg"
+              radius="md"
+              p={64}
+              variant="light"
+              className="text-center relative overflow-hidden"
+            >
+              <Image
+                id="commingSoon"
+                src={flowShape.src}
+                fit="contain"
+                alt="flowShape"
+                className={`${classes.flowShape} absolute top-0 left-0`}
+              />
+              <Text size="xl" fw="bold">
+                近日公開……！
+              </Text>
               <Text size="sm" mt={16} c="dimmed">
                 公開までしばらくお待ちください。
               </Text>
