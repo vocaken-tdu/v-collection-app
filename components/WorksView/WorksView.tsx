8a61e4a18f850e6a5fefabfb2b09e4d197c54524
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-24 03:51:38 +0900

    コメントリストの高さを左側要素とぴったり合わせる(CSS→Mantine hooksに)

--- components/WorksView/WorksView.tsx
+++ components/WorksView/WorksView.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useEffect } from 'react';
-import { SimpleGrid } from '@mantine/core';
+import { SimpleGrid, Text } from '@mantine/core';
+import { useElementSize } from '@mantine/hooks';
 import { BigImageCard } from '@/components/Card/BigImageCard';
 import { AuthorCard } from '@/components/Card/AuthorCard';
 import { CommentCard } from '@/components/Card/CommentCard';
@@ -10,6 +11,13 @@ import { useVerified, setVerified } from '@/store/verifiedStore';
 import classes from './WorksView.module.css';
 
 export function WorksView({ illustId }: { illustId: number }) {
+  // 各要素の高さを取得 → CommentCard の高さを決定
+  // 単に左側要素を取得するだけだと再帰して高さが変わることがある
+  // 安全のため refBIC + refAC に分けている
+  const { ref: refBIC, height: heightBIC } = useElementSize();
+  const { ref: refAC, height: heightAC } = useElementSize();
+  const { ref: refCF, height: heightCF } = useElementSize();
+
   // ドメイン名を取得
   useEffect(() => {
     // ドメイン認証
@@ -20,12 +28,29 @@ export function WorksView({ illustId }: { illustId: number }) {
   return (
     <SimpleGrid className={classes.wrap} cols={{ base: 1, sm: 2 }} spacing="lg">
       <SimpleGrid cols={1} spacing="md" className={classes.l}>
-        <BigImageCard illustId={illustId} />
-        <AuthorCard />
+        <div ref={refBIC}>
+          <BigImageCard illustId={illustId} />
+        </div>
+        <div ref={refAC}>
+          <AuthorCard />
+        </div>
       </SimpleGrid>
       <SimpleGrid cols={1} spacing="md" className={classes.r}>
-        <CommentCard illustId={illustId} isFormVisible={isAuth} />
-        {isAuth && <CommentForm illustId={illustId} />}
+        <CommentCard
+          illustId={illustId}
+          isFormVisible={isAuth}
+          // CommentCard の高さ = BigImageCard + AuthorCard - CommentForm
+          height={heightBIC + heightAC - heightCF}
+        />
+        <div ref={refCF}>
+          {isAuth ? (
+            <CommentForm illustId={illustId} />
+          ) : (
+            <Text ta="right" c="dimmed">
+              ※部員からのコメント
+            </Text>
+          )}
+        </div>
       </SimpleGrid>
     </SimpleGrid>
   );
