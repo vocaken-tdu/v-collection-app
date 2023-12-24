83020ac7e467cadc6adc2cf595e2a86dca6a6b01
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2023-12-25 02:30:35 +0900

    イラスト一覧ページのアニメーションを改善

--- components/Card/ImageCard.tsx
+++ components/Card/ImageCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Card, Text, Group, Image, Paper } from '@mantine/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -13,43 +13,11 @@ import flowShape from '@/public/flowShape.svg';
 gsap.registerPlugin(ScrollTrigger);
 
 export function ImageCard() {
-  const fadeRef = useRef(null);
+  // イラスト(リスト)の状態を取得
+  const illusts = useIllustList((state) => state.illustList);
 
-  // アニメーション
-  useLayoutEffect(() => {
-    // 上下に動くアニメーション
-    gsap.fromTo(
-      fadeRef.current,
-      {
-        y: 48,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 0.7,
-        stagger: 0.5,
-        ease: 'power2.out',
-        scrollTrigger: {
-          trigger: '#cards',
-          start: 'top 80%',
-          toggleActions: 'play none none reverse',
-        },
-      }
-    );
-    gsap.to(
-      '#commingSoon',
-      {
-        y: -100,
-        scrollTrigger: {
-          trigger: '#commingSoon',
-          start: 'top bottom',
-          end: 'bottom top',
-          scrub: 0.5,
-        },
-      }
-    );
-  });
+  // タグの状態を取得
+  const tags = useTags((state) => state.tags);
 
   // イラスト(リスト)を取得
   useEffect(() => {
@@ -57,11 +25,43 @@ export function ImageCard() {
     setTags();
   }, []);
 
-  // イラスト(リスト)の状態を取得
-  const illusts = useIllustList((state) => state.illustList);
-
-  // タグの状態を取得
-  const tags = useTags((state) => state.tags);
+  // アニメーション
+  useEffect(() => {
+    // 上下に動くアニメーション
+    if (document.getElementById('card')) {
+      gsap.fromTo(
+        '#card',
+        {
+          y: 48,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.7,
+          stagger: 0.05,
+          ease: 'power2.out',
+          scrollTrigger: {
+            trigger: '#cards',
+            start: 'top 80%',
+            toggleActions: 'play none none reverse',
+          },
+        }
+      );
+    }
+    // 公開予定の背景アニメーション
+    if (document.getElementById('commingSoon')) {
+      gsap.to('#commingSoon', {
+        y: -100,
+        scrollTrigger: {
+          trigger: '#commingSoon',
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: 0.5,
+        },
+      });
+    }
+  }, [illusts, tags]);
 
   return (
     <>
@@ -69,12 +69,12 @@ export function ImageCard() {
       {tags.map((tag, tagKey) => (
         <div key={tagKey + 100} className={classes.tag}>
           <h2 className="text-3xl flex justify-center mt-20 mb-8">{`― ${tag.name} ―`}</h2>
-          <div className={classes.cards} id="cards" ref={fadeRef}>
+          <div className={classes.cards} id="cards">
             {/* イラストをタグごとに表示 */}
             {illusts
               .filter((illust) => illust.tags.includes(tag.id))
               .map((illust, illustKey) => (
-                <div key={illustKey} className={classes.wrap}>
+                <div key={illustKey} className={classes.wrap} id="card">
                   <Card
                     p="lg"
                     shadow="lg"
