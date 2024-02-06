d54d8392afbebbb00d753044bd8f43296c88ec99
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-02-07 02:52:51 +0900

    [Mantine] カラーテーマをロゴカラーと統一

--- theme.ts
+++ theme.ts
@@ -1,8 +1,24 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineColorsTuple } from '@mantine/core';
+
+const blue: MantineColorsTuple = [
+  '#e9f5ff',
+  '#d7e6f9',
+  '#b2caea',
+  '#89acdb',
+  '#6693ce',
+  '#5084c7',
+  '#437cc5',
+  '#336aae',
+  '#295e9e',
+  '#16518d',
+];
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
   fontFamily: '"Noto Sans JP", sans-serif',
+  colors: {
+    blue,
+  },
 });
