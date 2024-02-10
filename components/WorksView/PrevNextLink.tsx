faac18c28133f84b252ba2bffe836d8fde5a0f9d
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-02-11 00:35:48 +0900

    イラストリストとタグデータを永続化(キャッシュ)し表示を高速化

--- components/WorksView/PrevNextLink.tsx
+++ components/WorksView/PrevNextLink.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
+import useStore from '@/store/useStore';
 import { useIllustList, setIllustList } from '@/store/illustListStore';
 import classes from './PrevNextLink.module.css';
 
@@ -13,7 +14,7 @@ export function PrevNextLink({ illustId }: { illustId: number }) {
   }, [illustId]);
 
   // 前後のイラストのIDを算出
-  const illusts = useIllustList((state) => state.illustList);
+  const illusts = useStore(useIllustList, (state) => state.illustList) || [];
   const index = illusts.findIndex((illust) => illust.id === illustId);
   const prevId = illusts[index - 1]?.id || undefined;
   const nextId = illusts[index + 1]?.id || undefined;
