25a29adfe7665dbfd5d73756c6c2f59c8484d9c3
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-01 21:02:29 +0900

    uiにIllustCardとNotificationsを作成しリファクタリング

--- components/Card/ImageCard.tsx
+++ components/Card/ImageCard.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import Link from 'next/link';
 import React, { useEffect } from 'react';
-import { Card, Text, Group, Image, Paper } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
+import { Text, Image, Paper } from '@mantine/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import useStore from '@/store/useStore';
-import { useIllustList, setIllustList, dataInfo } from '@/store/illustListStore';
+import { useIllustList, setIllustList } from '@/store/illustListStore';
+import { IllustCard } from '@/components/_ui/IllustCard';
 import { useTags, setTags } from '@/store/tagsStore';
-import { GetUserName } from '@/components/_tools/GetUserName';
 import classes from './ImageCard.module.css';
 import flowShape from '@/public/flowShape.svg';
 
@@ -18,24 +16,9 @@ gsap.registerPlugin(ScrollTrigger);
 export function ImageCard() {
   // イラスト(リスト)の状態を取得
   const illusts = useStore(useIllustList, (state) => state.illustList);
-  const isExist = useStore(dataInfo, (state) => state.isExist) || false;
 
   // タグの状態を取得
   const tags = useStore(useTags, (state) => state.tags);
-
-  // 画像が取得できなかったときの通知を表示
-  const fetchFailedImage = () => {
-    // イラストが存在する場合のみ通知を表示
-    isExist &&
-      notifications.show({
-        id: 'fetchFailedImage',
-        loading: true,
-        autoClose: false,
-        radius: 'md',
-        title: '画像が取得できませんでした。',
-        message: '現在対応中です。しばらくお待ちいただくか、長く続く場合はお問い合わせください。',
-      });
-  };
 
   // イラスト(リスト)を取得
   useEffect(() => {
@@ -93,26 +76,7 @@ export function ImageCard() {
             {illusts
               ?.filter((illust) => illust.tags.includes(tag.id))
               .map((illust, illustKey) => (
-                <div key={illustKey} className={classes.wrap} id="card">
-                  <Link href={`/works/${illust.id}`}>
-                    <Card p="lg" className={`big-shadow ${classes.card}`} radius="md" component="a">
-                      <Image
-                        className={classes.image}
-                        src={illust.illust}
-                        alt={illust.caption}
-                        onError={fetchFailedImage}
-                        loading="lazy"
-                      />
-                    </Card>
-                  </Link>
-                  <div className={`${classes.content} mt-2`}>
-                    <Group justify="space-between" gap="xs">
-                      <Text size="sm" className={classes.artist}>
-                        <GetUserName userId={illust.user_id} />
-                      </Text>
-                    </Group>
-                  </div>
-                </div>
+                <IllustCard illust={illust} illustKey={illustKey} />
               ))}
           </div>
           {/* イラストがない場合は公開予定であることを表示 */}
---
+++ components/_ui/IllustCard.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import { Card, Group, Image, Text } from '@mantine/core';
+import { NotifyFetchFailedImage } from '@/components/_ui/Notifications';
+import { dataInfo } from '@/store/illustListStore';
+import useStore from '@/store/useStore';
+import { GetUserName } from '@/components/_tools/GetUserName';
+import classes from './IllustCard.module.css';
+
+export function IllustCard({ illust, illustKey }: { illust: any; illustKey: number }) {
+  const isExist = useStore(dataInfo, (state) => state.isExist) || false;
+
+  // 画像が取得できなかったときの通知を表示
+  const fetchFailed = () => {
+    // イラストが存在する場合のみ実行
+    isExist && NotifyFetchFailedImage();
+  };
+
+  return (
+    <div key={illustKey} className={classes.wrap} id="card">
+      <Link href={`/works/${illust.id}`}>
+        <Card p="lg" className={`big-shadow ${classes.card}`} radius="md" component="a">
+          <Image
+            className={classes.image}
+            src={illust.illust}
+            alt={illust.caption}
+            onError={fetchFailed}
+            loading="lazy"
+          />
+        </Card>
+      </Link>
+      <div className={`${classes.content} mt-2`}>
+        <Group justify="space-between" gap="xs">
+          <Text size="sm" className={classes.artist}>
+            <GetUserName userId={illust.user_id} />
+          </Text>
+        </Group>
+      </div>
+    </div>
+  );
+}
