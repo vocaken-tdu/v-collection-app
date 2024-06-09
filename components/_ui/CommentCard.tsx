ccbd1e9996d4e1702fa98f66f765827eceec9207
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-09 18:48:10 +0900

    CommentCardにコメントを追記

--- components/_ui/CommentCard.tsx
+++ components/_ui/CommentCard.tsx
@@ -6,6 +6,12 @@ import { useLike, setLike } from '@/store/likeStore';
 import { GetRelativeTime } from '@/components/_tools/GetRelativeTime';
 import classes from './CommentCard.module.css';
 
+/*
+  コメントカード
+  ・コメントの内容 / ユーザー名 / 投稿日時 / いいねボタンと数 を表示
+  ・いいねの状態をローカルに保存 / クールダウンあり
+*/
+
 export function CommentCard({ comment, i }: { comment: any; i: number }) {
   // 処理中かどうかを記録
   const [isProcess, setProcess] = useState(false);
@@ -59,17 +65,22 @@ export function CommentCard({ comment, i }: { comment: any; i: number }) {
 
   return (
     <div key={i}>
+      {/* コメントカード */}
       <Paper px="xl" py="lg" radius="md" id="comment" className={`${classes.comment} light-shadow`}>
+        {/* コメントの内容 */}
         <Text size="sm">{comment.text}</Text>
         <div className="flex justify-between">
           <Group>
+            {/* ユーザー名 */}
             <Text pt="sm" fz="sm">
               {comment.user_name}
             </Text>
+            {/* 投稿日時 */}
             <Text pt="sm" fz="xs" c="dimmed" mt={1}>
               <GetRelativeTime RawTime={comment.created_at} />
             </Text>
           </Group>
+          {/* いいねボタン */}
           <Button
             variant="light"
             color="pink"
@@ -77,7 +88,9 @@ export function CommentCard({ comment, i }: { comment: any; i: number }) {
             onClick={switchLike(comment.id)}
             className={classes.button}
           >
+            {/* いいねの状態によってアイコンを変更 */}
             {isLiked(comment.id) ? <IconHeartFilled /> : <IconHeart />}
+            {/* いいねの数 */}
             {comment.like}
           </Button>
         </div>
