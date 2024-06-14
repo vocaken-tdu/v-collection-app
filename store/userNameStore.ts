b067b9dfa5966c5584f834a80f82c0f7f19eb4f1
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-14 19:49:58 +0900

    ユーザー名を永続化するように変更

--- store/userNameStore.ts
+++ store/userNameStore.ts
@@ -31,31 +31,51 @@ type userNameState = {
       name: string;
     },
   ];
+};
+
+export const useUserName = create<userNameState>()(
+  // 永続化オプション
+  persist(
+    () => ({
+      user: [
+        {
+          id: 0,
+          name: '',
+        },
+      ],
+    }),
+    {
+      name: 'userNameStore',
+    }
+  )
+);
+
+type dataInfo = {
+  // データが取得済みかどうか(重複取得を防ぐ)
   isExecuted: boolean;
 };
 
-export const useUserName = create<userNameState>()(() => ({
-  user: [
-    {
-      id: 0,
-      name: '',
-    },
-  ],
+export const dataInfo = create<dataInfo>(() => ({
   isExecuted: false,
 }));
 
 export const setUserName = async () => {
   const fetch = async () => {
-    const response: AxiosResponse = await axios.get(`${apiUrl}/user/`);
-    useUserName.setState({ user: response.data });
-    console.log('userName is fetched!');
+    try {
+      const response: AxiosResponse = await axios.get(`${apiUrl}/user/`);
+      useUserName.setState({ user: response.data });
+      console.log('userName is fetched!');
+    } catch (error) {
+      console.error('userName fetch failed!');
+    }
   };
   // 初めて実行するときのみ取得
-  useUserName.getState().isExecuted ? console.log('userName is already fetched!') : fetch();
-  useUserName.setState({ isExecuted: true });
+  const { isExecuted } = dataInfo.getState();
+  isExecuted ? console.log('userName is already fetched!') : fetch();
+  dataInfo.setState({ isExecuted: true });
 };
 
-// 補足: isFetched ではなく isExecuted を先行して使う理由
+// 補足: フェッチしたかではなく実行したか を使う理由
 // GetUserName.tsx は同時に複数実行される
 // その度にAPIを呼び出すのを回避する必要がある
 // ただし 実行したかどうか で判定すると fetch が正しく行われたかの状態によらないのが難点
