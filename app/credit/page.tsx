9ce94bd6e28c8c1a3a0d36c91dff7df51f6c2e56
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-16 03:37:42 +0900

    クレジットを一部書き換え

--- app/credit/page.tsx
+++ app/credit/page.tsx
@@ -1,28 +1,38 @@
-import { Container } from '@mantine/core';
+import { Container, SimpleGrid } from '@mantine/core';
+
+const credits = [
+  {
+    header: '企画',
+    members: ['うたの', 'ます', 'Rein', '紅シャケ', 'だいまおー。', 'Nyama', 'Harusame'],
+  },
+  {
+    header: 'フロントエンド開発',
+    members: ['もちきつね', 'Rein', 'あい', 'Ruto'],
+  },
+  {
+    header: 'バックエンド開発',
+    members: ['(問い合わせ中)', '(問い合わせ中)', 'ソフトウェア研究部 有志の方'],
+  },
+];
 
 export default function Home() {
   return (
     <>
       <Container size="xl" className="mt-20 pb-64">
-        <h2 className="text-4xl my-40">Credit</h2>
-        <p className="text-xl my-20 leading-loose">
+        <h2 className="text-4xl mt-40">Credit</h2>
+        <p className="text-xl mt-20 mb-40 leading-loose">
           このサイトは、VOCALOID同好会のメンバーと有志の方々によって運営されています。
         </p>
-        <h3 className="text-2xl mt-40">企画</h3>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <h3 className="text-2xl mt-20">フロントエンド開発</h3>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <h3 className="text-2xl mt-20">バックエンド開発</h3>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
-        <p className="text-xl leading-loose">暁さん</p>
+        <SimpleGrid cols={{ base: 1, sm: 3, lg: 3 }}>
+          {credits.map((credit) => (
+            <div>
+              <h3 className="text-2xl">{credit.header}</h3>
+              {credit.members.map((member) => (
+                <p className="text-xl leading-loose">{member}</p>
+              ))}
+            </div>
+          ))}
+        </SimpleGrid>
       </Container>
     </>
   );
