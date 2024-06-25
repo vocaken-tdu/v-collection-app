bc24e6280120f7810eef310e622632fcf0d92dc3
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-26 02:33:04 +0900

    PrevNextリンクを常に下に表示されるよう変更

--- components/_ui/PrevNextLink.tsx
+++ components/_ui/PrevNextLink.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import React, { useEffect } from 'react';
+import { Text } from '@mantine/core';
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 import useStore from '@/store/useStore';
 import { useIllustList, setIllustList } from '@/store/illustListStore';
@@ -25,10 +26,16 @@ export function PrevNextLink({ illustId }: { illustId: number }) {
       {prevId && (
         <Link href={`/works/${prevId}`} className={`${classes.arrow} ${classes.prev}`}>
           <IconChevronLeft size={48} stroke={1.5} color="var(--mantine-color-blue-filled)" />
+          <Text c="var(--mantine-color-blue-filled)" fw="bold">
+            前へ
+          </Text>
         </Link>
       )}
       {nextId && (
         <Link href={`/works/${nextId}`} className={`${classes.arrow} ${classes.next}`}>
+          <Text c="var(--mantine-color-blue-filled)" fw="bold">
+            次へ
+          </Text>
           <IconChevronRight size={48} stroke={1.5} color="var(--mantine-color-blue-filled)" />
         </Link>
       )}
