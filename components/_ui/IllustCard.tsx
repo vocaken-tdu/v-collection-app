9ddc6b567e9959a91866c76279d57c7a8446d897
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-06-27 03:29:30 +0900

    IntersectionObserverでイラスト一覧の各イラストが視界に入ったときにアニメーション発火するよう実装

--- components/_ui/IllustCard.tsx
+++ components/_ui/IllustCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Card, Group, Text } from '@mantine/core';
 import { NotifyFetchFailedImage } from '@/components/_tools/Notifications';
 import { dataInfo } from '@/store/illustListStore';
@@ -11,11 +11,43 @@ import classes from './IllustCard.module.css';
 export function IllustCard({ illust, i }: { illust: any; i: number }) {
   const isExist = useStore(dataInfo, (state) => state.isExist) || false;
   const [isLoaded, setIsLoaded] = useState(false);
+  const cardRef = useRef(null);
+  const [isIntersected, setIsIntersected] = useState(false);
+  const [isObserved, setIsObserved] = useState(false);
 
   // イラストが表示できたときの処理
   const onLoaded = () => {
     setIsLoaded(true);
   };
+
+  // IntersectionObserverの設定
+  useEffect(() => {
+    // イラストカードが存在しない場合は処理を終了
+    if (!cardRef.current) return;
+
+    // IntersectionObserverの設定
+    const options = {
+      rootMargin: '32px',
+    };
+
+    // IntersectionObserverで画像の読み込みを監視
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        // イラストが視野内に表示されたときの処理
+        if (entry.isIntersecting) {
+          setIsIntersected(true);
+          // (負荷軽減のため) 監視を終了
+          observer.disconnect();
+        }
+      });
+    }, options);
+
+    // イラストカードが存在していてまだ監視していない場合は監視を開始 (二重監視を防ぐ)
+    if (!isObserved) {
+      observer.observe(cardRef.current);
+      setIsObserved(true);
+    }
+  }, []);
 
   // 画像が取得できなかったときの通知を表示
   const fetchFailed = () => {
@@ -26,11 +58,11 @@ export function IllustCard({ illust, i }: { illust: any; i: number }) {
   return (
     <div
       key={i}
-      className={`${classes.wrap} ${isLoaded && 'anim-fadeUp'}`}
+      className={`${classes.wrap} ${isLoaded && isIntersected && 'anim-fadeUp'}`}
       style={{ animationDelay: `${i * 50}ms` }}
     >
       <Link href={`/works/${illust.id}`}>
-        <Card p="0" className={`big-shadow ${classes.card}`} radius="md">
+        <Card p="0" className={`big-shadow ${classes.card}`} radius="md" ref={cardRef}>
           <Image
             width={300}
             height={400}
